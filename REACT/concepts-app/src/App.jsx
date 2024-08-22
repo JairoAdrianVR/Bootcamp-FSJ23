b4ae72c25f13177ab1d8697b965357cf8532405f
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-08-22 17:21:35 -0600

    Clase React Conceptos Basicos - 21/8/24

---
+++ REACT/concepts-app/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import {Saludin} from './components/SaludinComponent'
+
+function App() {
+
+  return (
+   //Fragment -> <> </> DELIMITADORES CONTENEDORES
+  //Mostrar un componente en la vista, lo cargamos en el App.jsx
+
+    <>
+        <h1>Hola llegamos a React, vamo lo pibess</h1>
+        <h1>Hola llegamos a React, vamo lo pibess</h1>
+        <h1>Hola llegamos a React, vamo lo pibess</h1>
+        <h1>Hola llegamos a React, vamo lo pibess</h1>
+        <Saludin />
+    </>
+  )
+}
+
+export default App
+
+
---
+++ REACT/concepts-app/src/components/SaludinComponent.jsx
@@ -0,0 +1,13 @@
+//Creamos un nuevo componente JSX
+export function Saludin (){
+
+    return (
+    <>
+      <h1>Hola te saludo desde el saludin</h1>
+    </>
+    
+  )
+}
+
+
+  

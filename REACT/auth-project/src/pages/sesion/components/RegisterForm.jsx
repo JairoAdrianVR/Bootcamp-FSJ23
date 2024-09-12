a0d3fe4c4354169564d288e5c6f04937f10fa578
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-09-11 22:20:21 -0600

    Validacion YUP - React Hook Forms - Clase 11/9/24

--- REACT/auth-project/src/pages/sesion/components/RegisterForm.jsx
+++ REACT/auth-project/src/pages/sesion/components/RegisterForm.jsx
@@ -1,21 +1,33 @@
+import * as yup from 'yup'
+import {useForm} from 'react-hook-form'
+
+const schema = yup.object().shape({
+    email: yup.string().required("El correo es un campo obligatorio").email("Correo invalido. Ejemplo: asd@dominio.com"),
+    password: yup.string().required("La contrasenia es obligatoria.").min(8,"La contrasenia debe tener al menos 8 caracteres"),
+    confirmPassword: yup.string().oneOf([yup.ref('password'),null],"Las contrasenias deben coindicidir")
+})
+
 
 export const RegisterForm = () => {
+    const {register} = useForm();
+    
+//SpreadOperator 
   return (
     <>
         <form>
             <section>
                 <label>Email</label>
-                <input type="email" id="email" placeholder="Email for register" />
+                <input type="email" id="email" placeholder="Email for register" {...register('email')} />
             </section>
 
             <section>
                 <label>Password</label>
-                <input type="password" id="password" placeholder="Password for register" />
+                <input type="password" id="password" placeholder="Password for register" {...register('password')} />
             </section>
 
             <section>
                 <label>Confirm Password</label>
-                <input type="password" id="confirmPassword" placeholder="Repeat your password" />
+                <input type="password" id="confirmPassword" placeholder="Repeat your password" {...register('confirmPassword')} />
             </section>
         </form>
     </>

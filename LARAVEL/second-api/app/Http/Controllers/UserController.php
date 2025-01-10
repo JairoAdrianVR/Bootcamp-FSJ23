89cc2e0c05bc6ef0734927a0c522c3da4cbab696
JairoAdrianVR <jairoavegaromero@gmail.com>
2025-01-09 21:38:35 -0600

    Sanctum Login - Clase 9/1/25

--- LARAVEL/second-api/app/Http/Controllers/UserController.php
+++ LARAVEL/second-api/app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Exception;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -39,5 +40,46 @@ class UserController extends Controller
     }
 
     } //Aca cerramos la funcion register
+
+    public function login(Request $request){
+        try{
+
+            //Validate the request
+            $request->validate([
+                'email' => 'required|string|email',
+                'password' => 'required|string'
+            ]);
+
+            $credentials = $request->only('email','password');
+            
+            // Attempt to log the user in
+            // Auth::attempt() intenta loguear al usuario con las credenciales
+            // Pero su funcionamiento da true si el usuario se loguea correctamente
+            // con el ! negamos el resultado para que sea true si no se loguea
+            if(!Auth::attempt($credentials)){
+                // Si attempt no funciona damos una excepcion como respuesta
+                // y cortamos la ejecucion
+                throw new Exception('Invalid Login Credentials');
+            }            
+
+            // Si el email y el password estan bien obtenemos el usuario
+
+            $user = $request->user();
+
+            // Creamos un token para el usuario
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'User Login Successful!',
+                'user' => $user,
+                'token' => $token
+            ]);
+
+        } catch(Exception $e){
+            return response()->json([
+                'message' => "Login Failed! {$e->getMessage()}"
+            ] );
+        }
+    }
  
 }
--- LARAVEL/second-api/routes/api.php
+++ LARAVEL/second-api/routes/api.php
@@ -4,12 +4,13 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-*/
+
 
 Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
 
 //Route::post('/register','App\Http\Controllers\AuthController@register');
